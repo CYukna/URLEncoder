57487b2e2cfb8f9ebff3c6312ce80a6c324b8f6f
CYukna <42652935+CYukna@users.noreply.github.com>
2018-09-21 13:10:53 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,9 +4,54 @@ namespace URLEncoder
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool check = false;
+
+            try
+            {
+                Console.WriteLine("Enter the project name for your URL:");
+                string projectName = Console.ReadLine();
+
+                foreach (char c in projectName)
+                {
+                    if (c == ',')
+                        continue;
+                    if (c == '"')
+                        continue;
+                    break;
+                    Console.WriteLine("Invalid. Please enter another project name");
+                }
+
+                Console.WriteLine("Enter the activity name for your URL:");
+                string activityName = Console.ReadLine();
+
+                foreach (char c in activityName)
+                {
+                    if (c == ',')
+                        continue;
+                    if (c == '"')
+                        continue;
+                    break;
+                    Console.WriteLine("Invalid. Please enter another activity name");
+                }
+
+                string url = "https://companyserver.com/content/[project_name]/files/[activity_name]/[activity_name]Report.pdf";
+                Console.WriteLine("\n");
+                Console.WriteLine("url")
+
+                       var replace = url.Replace(" ", "%20");
+                Console.WriteLine(replace);
+            }
+
+            Console.WriteLine("Would you like another URL? y/n");
+            char ans = Convert.ToChar(Console.ReadLine());
+
+            if(ans=="y")
+            {
+                check = true;
+            }
+
         }
-    }
+    }    
 }
